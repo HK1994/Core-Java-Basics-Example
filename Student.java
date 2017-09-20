3ba7ef652e5b9edccdacba549a2d16398cacb2f4
Hemant Kumar <hemantkumarvs94@gmail.com>
2017-09-20 15:52:01 +0530

    Adding files for multi-column sorting

---
+++ Student.java
@@ -0,0 +1,15 @@
+package hemant.code.fileHandling.MultiColumnSortingInFile;
+
+/**
+ * Created by hemantkumar on 9/20/2017.
+ */
+public class Student {
+    String name;
+    int marks;
+
+    public Student(String name,int marks)
+    {
+        this.marks = marks;
+        this.name = name;
+    }
+}
