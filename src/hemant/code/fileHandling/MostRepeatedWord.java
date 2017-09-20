9ffb938386f40f5d4b55c77e8b899380a1bb1ffa
Hemant <hemantkumar@axway.com>
2017-09-20 14:19:49 +0530

    Updating Code with Java7

--- src/hemant/code/fileHandling/MostRepeatedWord.java
+++ src/hemant/code/fileHandling/MostRepeatedWord.java
@@ -1,5 +1,8 @@
 package hemant.code.fileHandling;
 
+import com.sun.media.jfxmedia.logging.Logger;
+import org.apache.commons.logging.Log;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,63 +10,42 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
 /**
  * Created by hemantkumar on 9/20/2017.
  * JAVA Program To Find The Most Repeated Word In Text File In Java.
  */
 public class MostRepeatedWord {
     public static void main(String[] args) throws IOException {
-        HashMap<String,Integer> wordCountMap = new HashMap<>();
-        BufferedReader bufferedReader= null;
-
-        try {
-            bufferedReader = new BufferedReader(new FileReader("C:\\CoreJavaCodes\\src\\hemant\\code\\fileHandling\\CountData.txt"));
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+        try (FileReader reader = new FileReader("C:\\CoreJavaCodes\\src\\hemant\\code\\fileHandling\\CountData.txt");
+             BufferedReader bufferedReader = new BufferedReader(reader)
+        ) {
             String currentLine = bufferedReader.readLine();
-            while (currentLine!= null)
-            {
+            while (currentLine != null) {
                 String[] wordCount = currentLine.toLowerCase().split(" ");
-                for (String word: wordCount) {
-
-                        if(wordCountMap.containsKey(word))
-                        {
-                            wordCountMap.put(word,wordCountMap.get(word)+1);
-                        }
-                        else
-                        {
-                            wordCountMap.put(word,1);
-                        }
+                for (String word : wordCount) {
+                    if (wordCountMap.containsKey(word)) {
+                        wordCountMap.put(word, wordCountMap.get(word) + 1);
+                    } else {
+                        wordCountMap.put(word, 1);
+                    }
                 }
                 currentLine = bufferedReader.readLine();
             }
 
             String mostRepeatedWord = null;
             int mostRepeatedWordCount = 0;
-
             Set<Map.Entry<String, Integer>> entrySet = wordCountMap.entrySet();
-
-            for (Map.Entry<String, Integer> entry : entrySet)
-            {
-                if(entry.getValue() > mostRepeatedWordCount)
-                {
+            for (Map.Entry<String, Integer> entry : entrySet) {
+                if (entry.getValue() > mostRepeatedWordCount) {
                     mostRepeatedWord = entry.getKey();
-
                     mostRepeatedWordCount = entry.getValue();
                 }
             }
-
-            System.out.println("The most repeated word in input file is : "+mostRepeatedWord);
-
-            System.out.println("Number Of Occurrences : "+mostRepeatedWordCount);
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                bufferedReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            System.out.println("The most repeated word in input file is : " + mostRepeatedWord);
+            System.out.println("Number Of Occurrences : " + mostRepeatedWordCount);
         }
     }
 }
+
